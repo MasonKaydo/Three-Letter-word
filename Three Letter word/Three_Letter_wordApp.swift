7fad8c8eef4ae34c5b3ace6623985e6213a4ddd8
mason <mkaydo@bsd220.org>
2025-11-04 10:31:35 -0600

    Initial Commit

---
+++ Three Letter word/Three_Letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_wordApp.swift
+//  Three Letter word
+//
+//  Created by mason kaydo on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
